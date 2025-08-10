fc1ff61c2ded661202b5daa3e266c5efb7471c2d
季同学 <2840724950@qq.com>
2025-08-10 22:33:14 +0800

    粒子动画

--- src/main.js
+++ src/main.js
@@ -1,14 +1,26 @@
-import './assets/main.css'
+import { createApp } from 'vue';
+import { createPinia } from 'pinia';
 
-import { createApp } from 'vue'
-import { createPinia } from 'pinia'
+import App from './App.vue';
+import router from './router';
 
-import App from './App.vue'
-import router from './router'
+// 重置样式表
+import "normalize.css";
 
-const app = createApp(App)
+// 粒子动画
+import Particles from "@tsparticles/vue3";
+import { loadSlim } from "@tsparticles/slim";
 
-app.use(createPinia())
-app.use(router)
 
-app.mount('#app')
+const app = createApp(App);
+
+app.use(createPinia());
+app.use(router);
+app.use(Particles, {
+    init: async engine => {
+        // await loadFull(engine);
+        await loadSlim(engine);
+    },
+});
+
+app.mount('#app');
---
+++ src/views/Home/index.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="home-container">
+        123
+    </div>
+
+</template>
+
+<script setup>
+import { onMounted } from "vue";
+
+</script>
+
+<style lang="less" scoped>
+.home-container {
+    width: 100%;
+    height: 100vh;
+
+}
+</style>
